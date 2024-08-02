6b8c3952ea6ac94838b0c448b26964a122c0d690
zhourengui <Nr@*y3FsBGg2>
2024-08-02 14:33:19 +0800

    Clear code

--- global.d.ts
+++ global.d.ts
@@ -1,20 +1,16 @@
-/// <reference path="../generated/proto/global_data_pb.ts" />
-/// <reference path="../generated/proto/micro_app_communication_pb.ts" />
+type DataListener = (data: Record<PropertyKey, unknown>) => void;
 
 interface MicroApp {
   addGlobalDataListener: (
-    listener: (data: GlobalData) => void,
+    listener: DataListener,
     autoTrigger?: boolean
   ) => void;
 
-  removeGlobalDataListener: (listener: (data: GlobalData) => void) => void;
+  removeGlobalDataListener: (listener: DataListener) => void;
 
-  addDataListener: (
-    listener: (data: MicroAppCommunication) => void,
-    autoTrigger?: boolean
-  ) => void;
+  addDataListener: (listener: DataListener, autoTrigger?: boolean) => void;
 
-  removeDataListener: (listener: (data: MicroAppCommunication) => void) => void;
+  removeDataListener: (listener: DataListener) => void;
 }
 
 interface Window {
